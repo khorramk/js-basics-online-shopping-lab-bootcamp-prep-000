32f194331692b6a03fd04663498ad6c1c739422f
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-20 23:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber === isNaN){
+  if(cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.");
   }
   
